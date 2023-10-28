9aee53b8c3b57df2eeb2f71f18fc4f13cbe8376e
MehdiRtal <mehdirtal7@pm.me>
2023-10-28 16:43:16 +0100

     

--- facebook.py
+++ facebook.py
@@ -42,7 +42,7 @@ class Facebook:
             raise Exception("CHECKPOINT")
         try:
             self._fb_dtsg = re.search(r'"DTSGInitialData":{"token":"(.*?)"', r.text).group(1)
-        except:
+        except Exception:
             raise Exception("LOGIN_FAILED")
 
     def _refresh_ad_act_id(self):
@@ -59,7 +59,7 @@ class Facebook:
         r = self._client.get("https://adsmanager.facebook.com/adsmanager/manage/accounts", headers=headers)
         self._ad_act_id = re.search(r"act=(\d+)", r.text).group(1)
 
-    def login(self, username: str = None, password: str = None, cookies: str = None):
+    def login(self, username: str = None, password: str = None, cookies: dict = None):
         if username and password:
             headers = {
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
@@ -104,18 +104,28 @@ class Facebook:
 
             self.cookies = dict(self._client.cookies)
         if cookies:
-            if ";" in cookies:
-                cookies = {c.split("=")[0].strip(): c.split("=")[1].strip() for c in cookies.split(";")}
-            else:
-                cookies = json.loads(cookies)
-                if type(cookies) == list:
-                    cookies = {c["name"]: c["value"] for c in cookies}
             self.cookies = cookies
             self._client.cookies.update(self.cookies)
 
         self._refresh_fb_dtsg()
 
-    def like(self):
+    def like(self, url: str):
+        headers = {
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "Accept-Encoding": "gzip, deflate",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Referer": "https://web.facebook.com/",
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Upgrade-Insecure-Requests": "1",
+                "Sec-Fetch-Dest": "document",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-Site": "same-origin",
+                "Sec-Fetch-User": "?1",
+        }
+        r = self._client.get(url, headers=headers)
+        feedback_id = re.search(r'Plugin","feedback_id":"(.*?)"', r.text).group(1)
+        feedback_id = "t"
+
         headers = {
             "Accept": "*/*",
             "Accept-Encoding": "gzip, deflate",
@@ -127,27 +137,98 @@ class Facebook:
             "Sec-Fetch-Site": "same-origin",
         }
         variables = json.dumps({
-            "input":{
-                "attribution_id_v2":"CometHomeRoot.react,comet.home,via_cold_start,1698005027853,771399,4748854339,",
-                "feedback_id":"ZmVlZGJhY2s6MjM2MTgyNDc4NzMzNjI2Ng==",
-                "feedback_reaction_id":"1635855486666999",
-                "feedback_source":"NEWS_FEED",
-                "is_tracking_encrypted":True,
-                "tracking":["AZVICwQDu_6CuVjivB0boF7oh9qyGyWQwJiVJrEJFBirvK95EFA9MNT24LkOORLrm1OJTGj1omKZwxWzV9QdyPtsZVyp-Vjko4NtjsCS-9eHO8JzG-l3Fm9dt6LlrnEhq_yIf2Mj10TrvVsDMyw3XKnTVv_PlNEWSaSxpJfUHVqFnflx6oizrDVSgbdMmqCL2pJY4EIcSCYDLfJ1-KP_zbmSym3_pW4QXs5CxzrmHAlbOr7K7nvIokq_kbXPJiikxp4XTduon0LVR66jy58ZgTfw5C6P6M_5198nnz7Z4SoTZGZ3qmMk40LBgeck8-XLajwGu-IwumEe_lEceHx8WcVQLqBH3UFmIwMgXUUZrdcUCsLsmy-2IrUh-6e8gYp97ou-uimtQf-ACNTeJjvCkgPUi_mgnWMuVT2EdOXdkRhL2skvxf1Q6nnhXyQdTd3BJDXzNFtG4MDphfR0AoRueDV2-mN4i4ODFJFJS9qi-KBP31gpDrW2pU7OB7Dun4ypIlZbbAOx_Y7sq7Tbggxhf692go_gax17xAfKv-8LvY5SB3ze1q0p1fD95hGjR2exnDoNsDpesMDLaH5kpwGoJ1TINavjynJWkweQWIBMxyupDASTbcpQlTpPb6zxJOovYfy068ArniXnfH6c1m_TO4YQX83xY-2UopZwnqnbmeZr4SrplcYkODrvOzEY9Z5ax1apfQpIkFd6xAFGYbCCvLzamPVNhJmnYs0WiDgouKRxu6Abut2QN1XMW94654MsBxpwiNDCS8ey63WWdZgBHh7hddJy14wqNKmwgdBL2D12pku-gAU69CiezCQSPNTEiiLpxNDyMgDyw21gKyaKa8Ikwshk_LNhi_1tG71gNrVz8MAFzerV6jUAfgI4zYu_hVmCHIbmcrQtHNcdsu8cXOTK___zyKMJonbqSsDBIwA0gYuGqrXYpJqekqxH6xPdv4F1dKF4nPb-waiSm4u6d0h-MY5JqI9tVy35J-gQQiNjUZmfzyDbla8fIGr4sJzMHErkldc9kbVSdEo_ZposU7ys7aMCRAKKzc0fKqOFCd0g4P6S5pIw-sRuVOvkbPjHQx_IrnpwBOj93CngmzfeKkBJnSnjsaUhmX0oymbo78vgPnfsuf2CidniWePuCXFEeAHy2H2GilDPDBPjR35KWnYbAJ-ChlXaFgG2vEJK7HA4VTczD5Y1V_FQCZoSUKuV0uqexnodNKnMS0fWSpO5Y-vnVlBQxRhfrCozmh-T0XlAjA"],
-                "session_id":"38cb90fb-e05d-4cba-9b80-08d2d1d9c0f2",
-                "actor_id":self.cookies.get("c_user"),
-                "client_mutation_id":"2"},
-            "useDefaultActor":False,
-            "scale":1
+            "input": {
+                "attribution_id_v2": "CometPhotoRoot.react,comet.mediaviewer.photo,via_cold_start,1698400320438,739435,,",
+                "feedback_id": feedback_id,
+                "feedback_reaction_id": "1635855486666999",
+                "feedback_source": "MEDIA_VIEWER",
+                "is_tracking_encrypted": True,
+                "tracking": [],
+                "session_id": str(uuid.uuid4()),
+                "actor_id": self.cookies.get("c_user"),
+                "client_mutation_id": "0"
+            },
+            "useDefaultActor": False,
+            "scale": 1
         })
         body = {
             "variables": variables,
             "doc_id": "6623712531077310",
             "fb_dtsg": self._fb_dtsg
         }
-        # body = f"variables={urllib.parse.quote(variables)}&doc_id=7250495224992687&fb_dtsg={urllib.parse.quote(self._fb_dtsg)}"
         r = self._client.post("https://web.facebook.com/api/graphql", headers=headers, data=body)
+        if r.json()["errors"]:
+            raise Exception("LIKE_FAILED")
 
+    def comment(self, url: str, text: str):
+        headers = {
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "Accept-Encoding": "gzip, deflate",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Referer": "https://web.facebook.com/",
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Upgrade-Insecure-Requests": "1",
+                "Sec-Fetch-Dest": "document",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-Site": "same-origin",
+                "Sec-Fetch-User": "?1",
+        }
+        r = self._client.get(url, headers=headers)
+        feedback_id = re.search(r'Plugin","feedback_id":"(.*?)"', r.text).group(1)
+
+        headers = {
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Referer": "https://web.facebook.com/",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+        }
+        variables = json.dumps({
+            "displayCommentsFeedbackContext": None,
+            "displayCommentsContextEnableComment": None,
+            "displayCommentsContextIsAdPreview": None,
+            "displayCommentsContextIsAggregatedShare": None,
+            "displayCommentsContextIsStorySet": None,
+            "feedLocation":"COMET_MEDIA_VIEWER",
+            "feedbackSource": 65,
+            "focusCommentID": None,
+            "groupID": None,
+            "includeNestedComments": False,
+            "input":{
+                "attachments": None,
+                "feedback_id": feedback_id,
+                "formatting_style": None,
+                "message":{
+                    "ranges": [],
+                    "text": text
+                },
+                "reply_target_clicked": False,
+                "attribution_id_v2": "CometPhotoRoot.react,comet.mediaviewer.photo,via_cold_start,1698403740008,170402,,",
+                "is_tracking_encrypted": True,
+                "tracking": ["{\"assistant_caller\":\"comet_above_composer\",\"conversation_guide_session_id\": None,\"conversation_guide_shown\": None}"],
+                "feedback_source": "MEDIA_VIEWER",
+                "idempotence_token": f"client:{str(uuid.uuid4())}",
+                "session_id": str(uuid.uuid4()),
+                "actor_id": self.cookies.get("c_user"),
+                "client_mutation_id": "0"
+            },
+            "inviteShortLinkKey": None,
+            "renderLocation": None,
+            "scale": 1,
+            "useDefaultActor": False,
+            "UFI2CommentsProvider_commentsKey": "CometPhotoRootQuery"
+        })
+        body = {
+            "variables": variables,
+            "doc_id": "6104498286317023",
+            "fb_dtsg": self._fb_dtsg
+        }
+        r = self._client.post("https://web.facebook.com/api/graphql", headers=headers, data=body)
+        if r.json()["errors"]:
+            raise Exception("COMMENT_FAILED")
 
     def call(self, number: str):
         capmonster = RecaptchaV2Task(self.capmonster_api_key)
@@ -177,7 +258,6 @@ class Facebook:
             "doc_id": "7250495224992687",
             "fb_dtsg": self._fb_dtsg
         }
-        # body = f"variables={urllib.parse.quote(variables)}&doc_id=7250495224992687&fb_dtsg={urllib.parse.quote(self._fb_dtsg)}"
         r = self._client.post("https://web.facebook.com/api/graphql", headers=headers, data=body)
         data = r.json()["data"]
         status = data["xfb_contact_removal_send_confirmation_code"]
@@ -215,7 +295,6 @@ class Facebook:
             "doc_id": "10055075961232407",
             "fb_dtsg": self._fb_dtsg
         }
-       #  body = f"variables={urllib.parse.quote(variables)}&doc_id=10055075961232407&fb_dtsg={urllib.parse.quote(self._fb_dtsg)}"
         r = self._client.post("https://adsmanager.facebook.com/api/graphql", headers=headers, data=body)
         serialized_state = r.json()["data"]["ixt_business_verification_advertiser_verification_wizard_trigger"]["screen"]["view_model"]["serialized_state"]
 
@@ -228,7 +307,7 @@ class Facebook:
                     "serialized_state": serialized_state
                 },
                 "actor_id": self.cookies.get("c_user"),
-                "client_mutation_id": "4"
+                "client_mutation_id": "0"
             },
             "scale": 1
         })
@@ -237,7 +316,6 @@ class Facebook:
             "doc_id": "6992119570822687",
             "fb_dtsg": self._fb_dtsg
         }
-        # body = f"variables={urllib.parse.quote(variables)}&doc_id=6992119570822687&fb_dtsg={urllib.parse.quote(self._fb_dtsg)}"
         r = self._client.post("https://adsmanager.facebook.com/api/graphql", headers=headers, data=body)
         serialized_state = r.json()["data"]["ixt_screen_next"]["view_model"]["serialized_state"]
 
@@ -248,7 +326,7 @@ class Facebook:
                     "serialized_state": serialized_state
                 },
                 "actor_id": self.cookies.get("c_user"),
-                "client_mutation_id": "5"
+                "client_mutation_id": "0"
             },
             "scale": 1
         })
@@ -257,7 +335,6 @@ class Facebook:
             "doc_id": "6992119570822687",
             "fb_dtsg": self._fb_dtsg
         }
-        # body = f"variables={urllib.parse.quote(variables)}&doc_id=6992119570822687&fb_dtsg={urllib.parse.quote(self._fb_dtsg)}"
         r = self._client.post("https://adsmanager.facebook.com/api/graphql", headers=headers, data=body)
         if not r.json()["data"]["ixt_screen_next"]:
             raise Exception("PHONE_VERIFICATION_FAILED")
