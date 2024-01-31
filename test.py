efaa6035c8836b83e2ddd24f1583dc0ed23c6832
MehdiRtal <mehdirtal7@pm.me>
2024-01-31 20:09:06 +0000

    Update Facebook login and account creation

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import json
 
 
 with Facebook() as fb:
-    fb.login(session=json.dumps({'ps_l': '0', 'ps_n': '0', 'fr': '08HHKF8BTuwghUGIe.AWXed7769IGehFlb-HXIZReJN2I.BluifL.iK.AAA.0.0.BluifL.AWVfNAuzAmw', 'sb': 'yye6ZT_auv395Xy_AHKuIU5G', 'c_user': '61556123128827', 'xs': '20%3AVcIyVn_iCyxFNg%3A2%3A1706698700%3A-1%3A-1', 'datr': 'yye6ZQqOkZreeB152IIx-I5t'}))
+    fb.login(session=json.dumps({'fr': '0MrXPdIeyIRSOgtkK.AWWrB2kBalDXPRQZugRGK0OtHWs.Blul1W.Tj.AAA.0.0.Blul1W.AWUTEcTJvtQ', 'sb': 'Vl26ZQ0PCD-_9LDABJhYaEmY', 'c_user': '61556142117202', 'xs': '29%3AgJksDVqAW--K1w%3A2%3A1706712407%3A-1%3A-1', 'datr': 'Vl26ZahaWjvLMUSN_pMFZvQK'}))
     fb.verify()
